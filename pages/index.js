3a5723506dbf7b34e9af9aa94c3dd8b807487992
Filip Johnsen <filipjohnsen97@gmail.com>
2020-09-10 23:30:43 +0200

    jdksdsd

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,61 @@
-import React from 'react'
 import Head from 'next/head'
-import { fetchEntries } from '../util/contentfulPosts'
+
+import { fetchEntries } from '../utils/contentfulPosts'
+
 import Post from '../components/Post'
 
-const Home = ({posts}) => (
-  <div>
-    <h1>Halla world</h1>
+export default function Home({ posts }) {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next + Contentful Starter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <h3>Hooray 🎉 - you've built this with <a href="https://nextjs.org">Next.js</a>!</h3>
-    <div className="posts">
-      {posts.map((p) => {
-        return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
-      })}
+      <main>
+        <div className="posts">
+          {posts.map((p) => {
+            return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
+          })}
+        </div>
+      </main>
+
+      <style jsx>{`
+        .container {
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        .posts {
+          display: flex;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
     </div>
-    <style jsx>{`
-      :global(html,body) {
-        margin: 0;
-        padding: 0;
-        height: 100%;
-      }
-
-      :global(body) {
-        font-size: calc(10px + 1vmin);
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
-          'Droid Sans', 'Helvetica Neue', sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        background-color: #282c34;
-        color: white;
-      }
-
-      a {
-        color: pink;
-        text-decoration: none;
-      }
-
-      .content {
-        padding: 0 32px;
-      }
-    `}</style>
-  </div>
-)
-
+  )
+}
 
 export async function getStaticProps() {
   const res = await fetchEntries()
@@ -61,5 +69,3 @@ export async function getStaticProps() {
     },
   }
 }
-
-export default Home
